7342a48da3dbd25864ae5b8ca3ceec016eb13d84
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-01-25 23:46:22 +0530

    Delete Text Intelligence directory

--- Text Intelligence/features/findNo.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def extract_mobile_numbers(text):
-    mobile_pattern = r'\b[6789]\d{9}\b'
-
-    mobile_numbers = re.findall(mobile_pattern, text)
-    return mobile_numbers
-    # print(mobile_numbers)
-
